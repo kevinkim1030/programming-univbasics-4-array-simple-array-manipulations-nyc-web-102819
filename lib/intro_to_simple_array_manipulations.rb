26d40c8da413f141404bebed862cf95a2a86389c
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 02:36:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,7 +45,7 @@ end
 
 list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
 def using_insert(list_of_programming_languages, another_language)
-  
+  new_array = list_of_programming_languages()
 
 
 
