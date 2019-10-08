f6ab6ac6f37377b55c2522236d9b6e792075d228
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 02:00:33 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, element)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  next_color = "violet"
+  using_push = colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, string)
+  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  new_neighborhood = "Staten Island"
+  using_unshift = bouroughs_in_nyc.unshift("Staten Island")
+end
+
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+def using_pop(continents)
+  using_pop = continents.pop
+end
+
+dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+def pop_with_args(dog_breeds)
+  small_dog = ["Chihuahua", "Shiba Inu"]
+  
+end
+
