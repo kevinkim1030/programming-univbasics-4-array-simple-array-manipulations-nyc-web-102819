845a094efcb8a825921727a158419100ef40cd85
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 02:59:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -63,7 +63,7 @@ describe "shift_with_args" do
 end
 
 
-def shift_with_args()
+def shift_with_args(ice_cream_brands)
   ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   brands_removed = ice_cream_brands.shift(2)
 end
