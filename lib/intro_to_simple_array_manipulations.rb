9f77dd3aee69b3953bb71ef214ec4d44eaba1af2
Kevin Kim <kevin.kim1030@gmail.com>
2019-10-08 02:28:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,11 +36,19 @@ end
 
 
 
+describe "using_flatten" do 
+  it "takes in an argument of an array that contains other arrays and uses the flatten method to return an array of strings" do 
+     instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+     flat_array = using_flatten(instruments)
+     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
+  end
+end
 
 
 
-
-
+instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+def using_flatten(instruments)
+  flattened_array = instruments.flatten
 
 
 
